8a5c9de724da2007beb3c665c25c8bd6aa1f0053
Renato França <renatofranca2881@gmail.com>
2023-01-03 17:49:47 -0300

    ATUAL01

--- src/app/nova-transferencia/nova-transferencia.componet.ts
+++ src/app/nova-transferencia/nova-transferencia.componet.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nova-tranferencia',
@@ -6,12 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-tranferencia.componet.scss'],
 })
 export class NovaTransferenciaComponet {
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor: number;
   destino: number;
 
   transferir() {
     console.log('Solicitada nova trasnsferencia');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+    this.limparFormulario();
+
+  }
+
+  limparFormulario(){
+    this.valor = 0;
+    this.destino = 0;
+
+
   }
 }
+
+
